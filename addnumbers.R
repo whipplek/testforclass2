4ac0543fc1a87ac3f1658f59568fef2794d7c40a
James Curley <jc3181@columbia.edu>
2018-11-07 11:40:25 -0600

    James added things

---
+++ addnumbers.R
@@ -0,0 +1,4 @@
+### Second file of code
+
+numbers <- c(2,2,3,7)
+sum(numbers)
--- code test.R
+++ code test.R
@@ -7,3 +7,13 @@ plot1 <- ggplot(iris, aes(x=Sepal.Length, y=Petal.Length, color = Species)) + ge
 
  
 ggsave("images/fig1.png", plot1, width=6, height=6)
+
+
+
+## second plot
+
+plot2 <- ggplot(iris, aes(x=Sepal.Length, y=Petal.Width, color = Species)) + geom_point()
+
+plot2
+
+ggsave("images/fig2.png", plot2, width=6, height=6)
